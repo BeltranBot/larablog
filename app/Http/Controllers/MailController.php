63ee36f5b4a2f99a88996a37a827e0ef7c4bf7c3
Carlos Beltran <canti89@gmail.com>
2015-09-14 12:24:02 -0500

    Initial Commit
    First commit

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Cinema\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mail;
+use Session;
+use Redirect;
+use Cinema\Http\Requests;
+use Cinema\Http\Controllers\Controller;
+
+class MailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        Mail::send('emails.contact',$request->all(), function($msj){
+            $msj->subject('Correo de Contacto');
+            $msj->to('palacios.raul02@gmail.com');
+        });
+
+        Session::flash('message','Mensaje enviado correctamente');
+        return Redirect::to('/contacto');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
